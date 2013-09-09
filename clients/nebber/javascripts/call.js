ca60e7a23d7acfbc6e45b11fcbb2156f35c69e9c
Sam Markiewicz <speakwithsam@gmail.com>
2013-09-09 14:33:56 +0200

    Landing

---
+++ clients/nebber/javascripts/call.js
@@ -0,0 +1,14 @@
+
+$('#m-link--scrollTo').smoothScroll({
+  easing: 'swing',
+  speed: 1200
+});
+
+$('#m-box--panel-1').parallax("bottom", 0.1, true);
+$('#m-box--panel-2').parallax("bottom", 0.4, true);
+
+skrollr.init({
+  forceHeight: false,
+  easing: 'sqrt'
+});
+
