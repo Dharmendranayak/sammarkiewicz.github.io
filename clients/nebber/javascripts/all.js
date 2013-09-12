b7a203a7fee2c059ce79b5c4045bba55dedd1464
Sammarkiewicz <speakwithsam@gmail.com>
2013-09-12 17:59:50 +0200

    test for tablets

--- clients/nebber/javascripts/all.js
+++ clients/nebber/javascripts/all.js
@@ -447,13 +447,13 @@ $('#m-link--scrollTo').smoothScroll({
   speed: 1200
 });
 
-// $('#m-box--panel-1').parallax("bottom", 0.1, true);
-// $('#m-box--panel-2').parallax("bottom", 0.4, true);
+$('#m-box--panel-1').parallax("bottom", 0.1, true);
+$('#m-box--panel-2').parallax("bottom", 0.4, true);
 
-skrollr.init({
-  forceHeight: false,
-  easing: 'sqrt'
-});
+// skrollr.init({
+//   forceHeight: false,
+//   easing: 'sqrt'
+// });
 
 $('.m-box--message').innerfade({
   speed: 'slow',
